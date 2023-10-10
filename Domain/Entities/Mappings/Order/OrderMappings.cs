6d1ba6b7551a081f660ec20905ce64fa2b40b5d2
Nguyễn Thái Bảo <baonguyenthai2@gmail.com>
2023-10-10 13:11:53 +0700

    fix

---
+++ Domain/Entities/Mappings/Order/OrderMappings.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Orders.Commands.DeleteOrder;
+using AutoMapper;
+using Domain.ViewModel.Orders;
+
+namespace Application.Mappings.Order
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<OrderVM, Domain.Entities.Order>();
+            CreateMap<OrderDetailVM, Domain.Entities.OrderDetail>();
+            CreateMap<DeleteOrderByIdCommand, Domain.Entities.OrderDetail>();
+        }
+    }
+}
